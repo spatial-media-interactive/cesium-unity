b3619c80cdf0d3e7e03a54eb5ec6424eb0b5a8b4
Kalvin Pearce <kalvin@Kalvins-MacBook-Pro.local>
2023-09-02 10:07:14 +1000

    fix: convert to generic texture loads

--- Reinterop~/GeneratedCppImplementationInvokerFunction.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Reinterop
-{
-    internal record GeneratedCppImplementationInvokerFunction(
-        string Content,
-        IEnumerable<CppType>? TypeDeclarationsReferenced = null,
-        IEnumerable<CppType>? TypeDefinitionsReferenced = null,
-        IEnumerable<string>? AdditionalIncludes = null)
-        : GeneratedCppElement(TypeDeclarationsReferenced, TypeDefinitionsReferenced, AdditionalIncludes);
-}
--- native~/Shared/src/Cesium.cpp
+++
@@ -1,8 +0,0 @@
-#include "Cesium.h"
-
-namespace CesiumForUnityNative {
-
-/*static*/ const std::string Cesium::version = "Development Build";
-/*static*/ const std::string Cesium::commit = "Unknown";
-
-} // namespace CesiumForUnityNative
